8db852c83ef1553d1e5842044dfa108d2f9f1551
Naoishe <McAleese-N6@ulster.ac.uk>
2024-03-05 14:00:50 +0000

    Dash Script Commented
    Code is unchanged but I forgot to add in the comments for the code so I've updated it to have comments now.

--- Assets/Scripts/DashScript.cs
+++ Assets/Scripts/DashScript.cs
@@ -4,91 +4,87 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
-    [SerializeField] private Rigidbody2D rb;
+    //Variables
+    private float horizontal;  //To hold horizontal anxis input
+    private float speed = 8f; //Movement Speed (I input a random value for testing but this can be changed without the code breaking)
+    private float jumpingPower = 16f; //Jumping power (also random assigned value)
+    private bool isFacingRight = true; //Boolean to determine player's facing direction . This is used in the 'Flip' function.
+    [SerializeField] private Rigidbody2D rb; //Serialized Fields to assign components
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private TrailRenderer tr;
-    private bool canDash = true;
-    private bool isDashing;
-    private float dashingPower = 30f;
-    private float dashingTime = 0.15f;
-    private float dashingCooldown = 3f;
-
-    private void Awake()
-    {
-
-    }
+    private bool canDash = true; //Testing Boolean to register if player can dash 
+    private bool isDashing; //Boolean to determine when the dash is being used
+    private float dashingPower = 30f; //Dashing Force (randomly assigned)
+    private float dashingTime = 0.15f; //Time that 'dash' takes to perform
+    private float dashingCooldown = 3f; //Dash cooldown counter
 
     private void Update()
     {
-        if (isDashing)
+        if (isDashing) //Prevents dash being spammed 
         {
             return;
         }
 
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal"); //Collecting horizontal axis input and assignbing it to the variable
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded()) //Testing if jump can be used
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) //Allows for different height of jumping if the player holds the key down for longer than just tapping it.
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) //Key assignment and trigger for the Dash ability
         {
             StartCoroutine(Dash());
         }
 
-        Flip();
+        Flip(); //Actively checks if 'Flip' needs to be used
     }
 
     private void FixedUpdate()
     {
-        if (isDashing)
+        if (isDashing) //Prevents dash being spammed 
         {
             return;
         }
 
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); //Makes the player move 
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer); //Returns whether the player is touching the ground or not
     }
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) //Tests player's facing direction
         {
             isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
+            Vector3 localScale = transform.localScale; //Flips player if facing left
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
     }
 
-    private IEnumerator Dash()
+    private IEnumerator Dash()  //Coroutine holding the Dash ability 
     {
-        canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        tr.emitting = false;
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
+        canDash = false; //Dash is being triggered so 'canDash' is temporarily disabled
+        isDashing = true; //Dash is being triggered so 'isDashing' is changed to true
+        float originalGravity = rb.gravityScale; //variaable to hold original gravity
+        rb.gravityScale = 0f; //Changes player's gravity to 0 temporarily so it doesn't impact 'dash'
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f); //Moves the player/performs the dash
+        tr.emitting = true; //Triggers trail renderer
+        yield return new WaitForSeconds(dashingTime); //Makes coroutine wait for the time dash is being performed
+        tr.emitting = false; //Deactivates the trail renderer after dash is done
+        rb.gravityScale = originalGravity; //Returns player's gravity back to normal
+        isDashing = false; //Disables 'isDashing'
+        yield return new WaitForSeconds(dashingCooldown); //Triggers cooldown of Dash
+        canDash = true; //After cooldown is complete, the player can dash again
 
     }
 }
